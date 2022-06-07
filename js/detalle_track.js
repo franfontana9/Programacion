b5271597f01926755ef2e4f6c853f9b3a89a1d94
JuliDaForno <jdaforno@udesa.edu.ar>
2022-06-07 18:09:19 -0300

    Corrección JS Index y detalle track

--- js/detalle_track.js
+++ js/detalle_track.js
@@ -1,8 +1,9 @@
 let queryString= location.search;
 let qsToObject = new URLSearchParams(queryString);
 let idTrack = qsToObject.get("id");
+console.log(idTrack);
 
-let urlDetalle = `https://api.deezer.com/0/track/${idTrack}`
+let urlDetalle = `https://api.allorigins.win/raw?url= http://api.deezer.com/chart/0/tracks/?id=${idTrack}`
 
 fetch(urlDetalle)
     .then(function(response){
