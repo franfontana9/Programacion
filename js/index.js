9f6a0fcac2fb5a274084e7445344198a4bd000e9
JuliDaForno <jdaforno@udesa.edu.ar>
2022-06-07 11:26:52 -0300

    Js index

--- js/index.js
+++ js/index.js
@@ -1,24 +1,26 @@
 //canciones
 
 
-let url = "https://api.allorigins.win/get?url = https://api.deezer.com/track/3135556 "
+let url = "https://api.allorigins.win/get?url=https://api.deezer.com/chart/0/tracks"
 
 fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data.contents);
-        let info = data.contents;
+        console.log(data);
+        let info = data;
 
-        let section = document.querySelector(".canciones");
+        let sectionCanciones = document.querySelector(".canciones");
         let article = "";
+         for (i=0; i<info[5]; i++){
             article += `<article>
-            <h4><a href="./detail-track.html?id=${info.id}">${info.title}</a></h4>
-                <p>${info.name}</p>
-                <img src="${info.picture}" height=150px >
+            <h4><a href="./detail-track.html?id=${info[i].id}">${info[i].title}</a></h4>
+            <p>${info[i].name}</p>
+            <img src="${info[i].picture} >
             </article>`
-        section.innerHTML = article
+         }
+         sectionCanciones.innerHTML = article
     })
     .catch(function(error){
         console.log(error);
