27d1687ea1290549ba911b40f02ac1e72ec3f1b4
JuliDaForno <jdaforno@udesa.edu.ar>
2022-06-08 14:55:39 -0300

    Avance JS index canciones

--- js/index.js
+++ js/index.js
@@ -9,21 +9,22 @@ fetch(url)
     })
     .then(function(data){
         console.log(data);
-        let info = data;
+        let info = data.data;
         let sectionCanciones = document.querySelector(".canciones");
         let lista= " ";
-        for (i=0; i<info[5]; i++){
+        
+        for (i=0; i<info.length; i++){
             lista += `<article>
-            <h4><a href="./detail-track.html?id=${info[i].album.id}">${info[i].album.title}</a></h4>
+            <h4><a href="./detail-track.html?id=${info[i].id}">${info[i].title}</a></h4>
             <p>${info[i].artist.name}</p>
-            <img src="${info[i].album.cover}>
+            <img src="${info[i].album.cover}">
             </article>`
          }
          console.log(lista);
          sectionCanciones.innerHTML = lista;
          sectionCanciones.style.display="flex";
-         sectionCanciones.style.flexDirection="row";
-         sectionCanciones.style.flexWrap="wrap";
+         sectionCanciones.style.flexDirection="column"
+         sectionCanciones.style.flexWrap="noWrap";
  
     })
     .catch(function(error){
@@ -31,12 +32,12 @@ fetch(url)
     })
 
 //Artistas
-//  let link = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/albums"
+ let album = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/albums"
  
-//  fetch(link)
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function(data){
-//         console.log(data)
-//     })
+ fetch(album)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
