d6bcccafd73e6d22f4f536bce383f940545da33f
JuliDaForno <jdaforno@udesa.edu.ar>
2022-06-09 14:49:29 -0300

    Agregar canción a favoritos

--- js/detalle_track.js
+++ js/detalle_track.js
@@ -19,27 +19,43 @@ fetch(urlDetalle)
         let track= document.querySelector(".track");
         let duracion = document.querySelector(".trackduration");
 
-        album.innerText= info.title;
-        foto.src= info.album;
-        artista_track.innerText= info.name;
-        track.innerText= info.title;
-        duracion.innerText=info.duration;
-
-
-
+        // album.innerText= info.title;
+        // foto.src= info.album;
+        // artista_track.innerText= info.name;
+        // track.innerText= info.title;
+        // duracion.innerText=info.duration;
+        
     // Favoritos
-        // let cancionesFavoritas = [];
-        // let recuperoStorage = localStorage.getItem("favoritos");
-        
-        // if(recuperoStorage){
-        //     let cancionesArray = JSON.parse(recuperoStorage)
-        //     cancionesFavoritas = cancionesArray
-        
-        // }
-        // let agregarCancion = document.querySelector(".icono");
-        // if(cancionesFavoritas.includes(idTrack)){
-        //     agregarCancion = `<i class=${"fa-solid fa-check"}></i>`
-        // }
+    let cancion = document.querySelector(".cancion");
+    let cancionesFavoritas = [];
+
+    
+    cancion.addEventListener("click", function(evento){
+        evento.preventDefault()
+
+
+        if(cancionesFavoritas.includes(idTrack)){
+            let sacarCancion = cancionesFavoritas.indexOf(idTrack)
+            cancionesFavoritas.splice(sacarCancion, 1);
+            cancion.innerText = "Agregar canción a mi playlist"
+        }
+        else{
+            cancionesFavoritas.push(idTrack);
+            cancion.innerText= "Sacar canción de mi playlist"
+        }
+
+        let cancionesFavoritasString = JSON.stringify(cancionesFavoritas)
+        localStorage.setItem("cancionesFavoritas", cancionesFavoritasString);
+        let recuperoLista = localStorage.getItem("cancionesFavoritas");
+        if(recuperoLista){
+            let cancionesArray = JSON.parse(recuperoLista)
+            cancionesFavoritas = cancionesArray
+        }
+    })
+
+    })
+    .catch(function(error){
+        console.log(error)
     })
 
 
