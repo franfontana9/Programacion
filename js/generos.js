7c68a8c2aa1d302c9914f47cce6d34b56633cfd8
franfontana9 <fontanaf@udesa.edu.ar>
2022-06-10 09:34:33 -0300

    avances fran

--- js/generos.js
+++ js/generos.js
@@ -1,4 +1,4 @@
-let url = "https://developers.deezer.com/api/genre= https://api.deezer.com/genre"
+let url = "https://api.allorigins.win/raw?url= https://api.deezer.com/genre"
 
 fetch (url)
     .then(function(response){
@@ -6,9 +6,29 @@ fetch (url)
     })
     .then(function(data){
         console.log(data);
+        let info = data.data
+        let section = document.querySelector(".ffsection");
+
+        let articles = "";
+
+        for(let i=1 ; i< 4; i++){
+            
+            articles += `<article class= "ffarticle">
+                            <p class="pgeneros">Nombre: ${data.data[i].title}</p>
+                            <img class="image" src="${data.data[i].pictures}" alt="">
+                            <a class="ffcul" href="detalledelgenero.html?id=${data.data[i].id}">${data.data[i].name}</a>
+                        </article>`
+
+        }
+
+        console.log(articles);
         
- 
+        section.innerHTML = articles
+
+
+
     })
+
     .catch(function(error){
         console.log(error);
     })
