aba45e17e559c7b08ceb859c84c360757ece4cd8
JuliDaForno <jdaforno@udesa.edu.ar>
2022-06-16 14:48:14 -0300

    Búsqueda Cargando

--- js/search-results.js
+++ js/search-results.js
@@ -30,10 +30,10 @@ let link = `https://api.allorigins.win/raw?url=https://api.deezer.com/search?q=$
 
     
 fetch (link)
-    .then(function (response) {
+    .then(function(response) {
         return response.json();
     })
-    .then(function (data) {
+    .then(function(data) {
         let info = data.data
         let results = document.querySelector('.content_search');
         let busqueda =""
@@ -47,10 +47,12 @@ fetch (link)
             }
         results.innerHTML= busqueda
         } 
-        else if(info.length=0){
-                results.innerHTML=`
-                <h1 class="artistas3">No hay resultados</h1> 
-                `
+        else if(info.length==0){
+                busqueda =`
+                <article class="content_search">
+                <h1 class="h1_sr">No hay resultados coincidentes</h1>
+            </article>`
+        results.innerHTML = busqueda
         } 
         
 
